27991f0e5ebc07036ef5852ddbc6f3b24d7b0184
Miguel Angel Zanguna Hurtado <miguel.zanguna@sofka.com.co>
2022-06-08 20:51:35 -0500

    Hola Mundo

---
+++ src/main/java/com/sofkamarket/HolaMundoController.java
@@ -0,0 +1,15 @@
+package com.sofkamarket;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/base")
+public class HolaMundoController {
+
+    @GetMapping("/v1")
+    public String saludar() {
+        return "Hola Mundo";
+    }
+}
---
+++ src/main/java/com/sofkamarket/SofkaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sofkamarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofkaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofkaMarketApplication.class, args);
+	}
+
+}
